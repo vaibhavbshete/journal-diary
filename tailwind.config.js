2f1803ef76973ad999fd1d0c2cc0227c82983731
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-07-30 22:58:46 +0530

    Make Hind the sans default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['Hind',...defaultTheme.fontFamily.sans], 
+       }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
