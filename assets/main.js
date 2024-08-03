3c066556e8cf210b873cf7b6d6265369af9c86b1
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-08-04 01:07:39 +0530

    fix: add padding to date in reset function

--- assets/main.js
+++ assets/main.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
         noteInp.value = ''
         
         let dt = new Date()
-        let dtStr = dt.getFullYear() + '-' + ((dt.getMonth() + 1).toString().padStart(2, '0')) + '-' + dt.getDate() + 'T' + (dt.getHours().toString().padStart(2, '0')) + ':' + (dt.getMinutes().toString().padStart(2, '0'))
+        let dtStr = dt.getFullYear() + '-' + ((dt.getMonth() + 1).toString().padStart(2, '0')) + '-' + dt.getDate().toString().padStart(2,0) + 'T' + (dt.getHours().toString().padStart(2, '0')) + ':' + (dt.getMinutes().toString().padStart(2, '0'))
         // console.log(dtStr);
         startInp.value = dtStr
     }
