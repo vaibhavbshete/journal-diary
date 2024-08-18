333da7a0dccbbac208b2cf3d14ff207e0f0dda4a
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-08-18 10:29:00 +0530

    reset function to set date-only in date input

--- assets/main.js
+++ assets/main.js
@@ -258,9 +258,8 @@ document.addEventListener('DOMContentLoaded', () => {
         noteInp.value = ''
         
         let dt = new Date()
-        let dtStr = dt.getFullYear() + '-' + ((dt.getMonth() + 1).toString().padStart(2, '0')) + '-' + dt.getDate().toString().padStart(2,0) + 'T' + (dt.getHours().toString().padStart(2, '0')) + ':' + (dt.getMinutes().toString().padStart(2, '0'))
-        // console.log(dtStr);
-        startInp.value = dtStr
+        let dp = getDateParts(dt)
+        startInp.value = dp.ymd
     }
 
     function displayFormDateTime() {
